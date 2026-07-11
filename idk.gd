ef84619e6dcbbb195944d2ebc8dabd4ac7ed724d
10539ws <vernonheng2012@gmail.com>
2026-07-11 13:13:36 +0800

    9

--- idk.gd
+++ idk.gd
@@ -1,19 +1,15 @@
-
 extends Control
-
-var coffee_bean = noel
 
 @onready var label :Label =$Label
 @onready var button:Button = $Button
-@onready var times :int = 0
+@onready var times :int = 1
 @onready var runtime :int = 10
+
 func _process(_delta: float) -> void:
 	button.pressed.connect(_on_button_pressed)
 
 func _on_button_pressed() -> void:
 	label.text = "time pressed ="+str(times)
 	times += 1
-	if times >= runtime:
+	if times > runtime:
 		label.text = "time pressed = 10"
-		if coffee_bean 
-		
