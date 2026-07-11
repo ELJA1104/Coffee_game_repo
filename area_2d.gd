a55cfd60ed94a971843e370bedad693ac0976fff
noellllllllllllllllll <m-12422729@moe-dl.edu.my>
2026-07-11 14:51:13 +0800

    Update area_2d.gd

--- area_2d.gd
+++ area_2d.gd
@@ -1,5 +1,7 @@
 extends Area2D
-signal teleport_requested(position_Grinder: Vector2)
+signal teleport_requested(position_Grinder: Vector2) 
+#noel's
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("bean"):
 	
