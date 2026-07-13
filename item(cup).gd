77dc7a9e04aee82f9dddd398fb43e2cc2c50451d
10539ws <vernonheng2012@gmail.com>
2026-07-14 00:57:43 +0800

    Revert "a"
    This reverts commit 911227310142f804bdc30a87f9a6649f90b1b91a, reversing
    changes made to b9e10e7cc51431fa1d710725782136016dd69cc3.

--- item(cup).gd
+++ item(cup).gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
-var when_is_grab_coffe_bean : bool = false
+var when_is_grab_cup : bool = false
 
 func _process(_delta):
-	if when_is_grab_coffe_bean:
+	if when_is_grab_cup:
 		var _mouse_pos = get_global_mouse_position()
 		global_position = lerp(global_position,_mouse_pos,0.2)
 		return
@@ -11,12 +11,18 @@ func _process(_delta):
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
-			when_is_grab_coffe_bean = true
+			when_is_grab_cup = true
 		else:
-			when_is_grab_coffe_bean = false
+			when_is_grab_cup = false
 #========================================================================================
 
 
+
+
+	
+	
+	
+	
 	
 	
 
