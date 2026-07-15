2156d52aa326f1cb7f2db79fcee39cf2364fea13
10539ws <vernonheng2012@gmail.com>
2026-07-15 21:02:22 +0800

    a

--- Code/idk.gd
+++ Code/idk.gd
@@ -1,8 +1,31 @@
+extends Node2D
 
+var coffee_entered :bool = false
 
+@onready var button:Button = $Button
+@onready var times :int = 1
+@onready var runtime :int = 10
+@onready var cup :Node2D = $cup
+@onready var label :Label = $Label
+signal tp_cup
 
-
-
+func _process(_delta: float) -> void:
+	button.pressed.connect(_on_button_pressed)
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	if body.is_in_group("coffe bean"):
+		coffee_entered = true
+	else :
+		coffee_entered = false
+
+func _on_button_pressed() -> void:
+	label.text = "Pressed time =" +str(times)
+	times += 1
+	if coffee_entered:
+		if times > runtime:
+			tp_cup.emit()
+			tp_cup.connect(cup.teleport_cup_to_grinder)
+			label.text = "Congrat" 
+	else:
+		if  times > runtime:
+			label.text = "Max"
