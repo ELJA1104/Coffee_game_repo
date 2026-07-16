8a496027b325d242fc7ee60b3a2cba115fb476d3
noellllllllllllllllll <m-12422729@moe-dl.edu.my>
2026-07-16 20:48:19 +0800

    l

--- Code/item(powder).gd
+++ Code/item(powder).gd
@@ -26,3 +26,16 @@ func _on_mouse_entered():
 func _on_mouse_exited():
 	mouse_inside_powder = false
 #==========================================================================================
+func _on_teleport_request(tp_powder, pos):
+	if tp_powder == "tp_to_powder":
+		global_position = pos
+
+
+
+
+
+	
+signal teleport_request(tp_powder: String, pos: Vector2)
+	
+func 放ready():
+	teleport_request.emit("tp_to_powder", global_position)
